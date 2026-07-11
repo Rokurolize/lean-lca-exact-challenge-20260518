cf003cc9497131d613274502004ed0f42d08b6ab
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-07-12 05:42:03 +0900

    repair slice vertex indexing

--- LeanLCAExactChallenge/Infinity/JoyalSlices.lean
+++ LeanLCAExactChallenge/Infinity/JoyalSlices.lean
@@ -16,31 +16,31 @@ namespace LeanLCAExactChallenge.Infinity
 open CategoryTheory Opposite Simplicial
 
 /-- The representable under-slice at a vertex. -/
-abbrev underSlice (Q : SSet.{u}) (z : Q _⟦0⟦) : SSet.{u} :=
+abbrev underSlice (Q : SSet.{u}) (z : Q _⦋0⦌) : SSet.{u} :=
   relativeDaySliceOverMap
     (emptyAugmentation.{u}.obj (Δ[0] : SSet.{u}))
     (emptyAugmentation.{u}.obj Q)
     (emptyAugmentation.{u}.map (SSet.yonedaEquiv.symm z))
 
 /-- The universal fixed-base cone represented by the under-slice. -/
-def underSliceUniversalCone (Q : SSet.{u}) (z : Q _⟦0⟦) :=
+def underSliceUniversalCone (Q : SSet.{u}) (z : Q _⦋0⦌) :=
   (relativeDaySliceOverMapFixedBaseEquiv
     (emptyAugmentation.{u}.obj (Δ[0] : SSet.{u}))
     (emptyAugmentation.{u}.obj Q) (underSlice Q z)
     (emptyAugmentation.{u}.map (SSet.yonedaEquiv.symm z))) ( 𝟙 _)
 
 /-- Forget the fixed initial vertex of an under-slice simplex. -/
-def underSliceProjection (Q : SSet.{u}) (z : Q _⟦0⟦) :
+def underSliceProjection (Q : SSet.{u}) (z : Q _⦋0⦌) :
     underSlice Q z ⟶ Q :=
   simplicialJoinRightInclusion (Δ[0] : SSet.{u}) (underSlice Q z) ≫
     forgetAugmentation.{u}.map (underSliceUniversalCone Q z).1
 
 /-- The representable over-slice, obtained by reversing an under-slice. -/
-abbrev overSlice (Q : SSet.{u}) (z : Q _⟦0⟦) : SSet.{u} :=
+abbrev overSlice (Q : SSet.{u}) (z : Q _⦋0⦌) : SSet.{u} :=
   (underSlice Q.op z).op
 
 /-- Forget the fixed final vertex of an over-slice simplex. -/
-def overSliceProjection (Q : SSet.{u}) (z : Q _⟦0⟦) :
+def overSliceProjection (Q : SSet.{u}) (z : Q _⦋0⦌) :
     overSlice Q z ⟶ Q :=
   SSet.opFunctor.map (underSliceProjection Q.op z) ≫
     (SSet.opFunctorCompOpFunctorIso.app Q).hom
