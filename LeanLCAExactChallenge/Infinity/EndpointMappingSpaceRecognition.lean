aab4283fdc104312ffa8a3a4055f33b459b7e8ab
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-07-17 03:14:20 +0900

    Assemble endpoint mapping-space recognition

--- LeanLCAExactChallenge/Infinity/EndpointMappingSpaceRecognition.lean
+++ LeanLCAExactChallenge/Infinity/EndpointMappingSpaceRecognition.lean
@@ -6,6 +6,7 @@ Authors: Rokurolize
 
 import LeanLCAExactChallenge.Infinity.EndpointMappingSpaces
 import LeanLCAExactChallenge.Infinity.OrdinaryLocalizationBridge
+import LeanLCAExactChallenge.Infinity.BicategoricalEquivalenceInternalHom
 
 /-! # Recognition infrastructure for endpoint mapping spaces -/
 
@@ -18,7 +19,8 @@ universe u
 namespace LeanLCAExactChallenge
 namespace Infinity
 
-open CategoryTheory Opposite Simplicial
+open CategoryTheory CategoryTheory.MonoidalClosed Opposite Simplicial
+open scoped Bicategory MonoidalCategory MonoidalClosed
 
 /-- Chosen object representatives and equivalence edges in the counit orientation. -/
 structure EndpointFiberObjectChoice
@@ -52,6 +54,59 @@ theorem endpointFiberEssentialSurjectivityChoices
     fun y ↦ (hchoice y).choose_spec.choose,
     fun y ↦ (hchoice y).choose_spec.choose_spec
   ⟩⟩
+
+/-- Local equivalence of every fixed-endpoint mapping-space map. -/
+def EndpointFiberLocalEquivalenceInput
+    {X Y : SSet.QCat.{u}} (F : X ⟶ Y)
+    (hX : ∀ x y : X.obj _⦋0⦌, SSet.Quasicategory (endpointFiber X.obj x y))
+    (hY : ∀ x y : Y.obj _⦋0⦌, SSet.Quasicategory (endpointFiber Y.obj x y)) : Prop :=
+  ∀ x y : X.obj _⦋0⦌,
+    IsBicategoricalEquivalence
+      (endpointFiberQCatMap F x y (hX x y)
+        (hY (F.hom.app (op ⦋0⦌) x) (F.hom.app (op ⦋0⦌) y)))
+
+/-- Coherent quasi-inverse data sufficient for a bicategorical adjoint equivalence. -/
+structure EndpointFiberQuasiInverseData
+    {X Y : SSet.QCat.{u}} (F : X ⟶ Y) where
+  inverse : Y ⟶ X
+  unitEdge :
+    SSet.Edge
+      (SSet.unitHomEquiv ((ihom X.obj).obj X.obj)
+        (MonoidalClosed.curry' ((𝟙 X : X ⟶ X).hom)))
+      (SSet.unitHomEquiv ((ihom X.obj).obj X.obj)
+        (MonoidalClosed.curry' (F ≫ inverse).hom))
+  unitEdge_isEquivalence : EdgeIsEquivalence unitEdge
+  counitEdge :
+    SSet.Edge
+      (SSet.unitHomEquiv ((ihom Y.obj).obj Y.obj)
+        (MonoidalClosed.curry' (inverse ≫ F).hom))
+      (SSet.unitHomEquiv ((ihom Y.obj).obj Y.obj)
+        (MonoidalClosed.curry' ((𝟙 Y : Y ⟶ Y).hom)))
+  counitEdge_isEquivalence : EdgeIsEquivalence counitEdge
+
+/-- Coherently realizing object choices and local inverses produces quasi-inverse data. -/
+def EndpointFiberCoherentQuasiInverseContract : Prop :=
+  ∀ {X Y : SSet.QCat.{u}} (F : X ⟶ Y)
+    (hX : ∀ x y : X.obj _⦋0⦌, SSet.Quasicategory (endpointFiber X.obj x y))
+    (hY : ∀ x y : Y.obj _⦋0⦌, SSet.Quasicategory (endpointFiber Y.obj x y))
+    (_choices : EndpointFiberObjectChoice F),
+    EndpointFiberLocalEquivalenceInput F hX hY →
+      Nonempty (EndpointFiberQuasiInverseData F)
+
+/-- Coherent endpoint-fiber quasi-inverses imply the endpoint recognition contract. -/
+theorem endpointFiberRecognition_of_coherentQuasiInverse
+    (hcoherent : EndpointFiberCoherentQuasiInverseContract.{u}) :
+    EndpointFiberRecognitionContract.{u} := by
+  intro X Y F hX hY hess hlocal
+  obtain ⟨choices⟩ := endpointFiberEssentialSurjectivityChoices F hess
+  obtain ⟨data⟩ := hcoherent F hX hY choices hlocal
+  letI : Category (X ⟶ X) := SSet.QCat.bicategory.homCategory X X
+  letI : Category (Y ⟶ Y) := SSet.QCat.bicategory.homCategory Y Y
+  let unit : (𝟙 X) ≅ F ≫ data.inverse :=
+    twoIsoOfInternalHomEquivalenceEdge data.unitEdge data.unitEdge_isEquivalence
+  let counit : data.inverse ≫ F ≅ 𝟙 Y :=
+    twoIsoOfInternalHomEquivalenceEdge data.counitEdge data.counitEdge_isEquivalence
+  exact ⟨Bicategory.Equivalence.mkOfAdjointifyCounit unit counit, rfl⟩
 
 end Infinity
 end LeanLCAExactChallenge
